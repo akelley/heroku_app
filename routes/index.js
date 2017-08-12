caf837b5279f6720b8bc759e6df3c1fe0bca39b4
Andrew Kelley <iodine73@gmail.com>
2017-08-12 12:57:07 -0400

    clean up too many index.js files

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,14 @@ var Twitter = require('twitter');
 
 var router = express.Router(); 
 var client = new Twitter({
-  consumer_key: process.ENV.CONSUMER,
-  consumer_secret: process.ENV.CONSUMER_SECRET,
-  access_token_key: process.ENV.TOKEN,
-  access_token_secret: process.ENV.TOKEN_SECRET
+  consumer_key: process.env.CONSUMER,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.TOKEN,
+  access_token_secret: process.env.TOKEN_SECRET
 });
 
 router.get('/', function(req, res, next){  // https://dev.twitter.com/rest/reference/get/statuses/user_timeline
   var senID = req.query.q;
-  console.log(req);
   client.get('statuses/user_timeline', { screen_name: senID, count: 5 }, function(error, tweets, response) {
     
     if (!error) {
