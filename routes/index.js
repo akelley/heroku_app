9a4070f4b82f3645c2a7d03e066082df8862143e
Andrew Kelley <iodine73@gmail.com>
2017-08-12 12:51:09 -0400

    clean up too many index.js files

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,23 @@ var Twitter = require('twitter');
 
 var router = express.Router(); 
 var client = new Twitter({
-  consumer_key: 'UHbO3pcfmfhPHQNFMmEN7E6Jz',
-  consumer_secret: 'IQHed5dQgYQyPDKLSqwhjO5K4olicuSiNfOsvRcKWjfa8nidFO',
-  access_token_key: '894950779215384577-kmLQp1EK179RvQSXNVjGL5eh5lCzCcJ',
-  access_token_secret: 'rPBeCQN93Dl7BHfCcE2igRFVZMbAZOmQEKFXCb2nH2VH4'
+  consumer_key: process.ENV.CONSUMER,
+  consumer_secret: process.ENV.CONSUMER_SECRET,
+  access_token_key: process.ENV.TOKEN,
+  access_token_secret: process.ENV.TOKEN_SECRET
 });
 
 router.get('/', function(req, res, next){  // https://dev.twitter.com/rest/reference/get/statuses/user_timeline
-  client.get('statuses/user_timeline', { screen_name: 'SenWarren', count: 5 }, function(error, tweets, response) {
+  var senID = req.query.q;
+  console.log(req);
+  client.get('statuses/user_timeline', { screen_name: senID, count: 5 }, function(error, tweets, response) {
+    
     if (!error) {
       //res.status(200).render('index', { title: 'Express', tweets: tweets });
-      res.status(200).json({tweets});
+      res.status(200).jsonp({ tweets });
     }
     else {
-      res.status(500).json({ error: error });
+      res.status(500).jsonp({ error: error });
     }
   });
 });
